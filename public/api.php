7c16cebadc09eaa1f50f249ea55d0cee7a11f0b3
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2019-05-16 00:02:33 +0200

    Add housing model with endpoint on awd lambda :rocket:

--- public/api.php
+++ public/api.php
@@ -2,9 +2,24 @@
 
 declare(strict_types=1);
 
+namespace PhpmlLambda;
+
+use Phpml\ModelManager;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
 switch (true) {
     case match('iris'):
         echo 'iris';
+        break;
+    case match('housing'):
+        $model = (new ModelManager())->restoreFromFile(__DIR__.'/../model/housing.phpml');
+        echo json_encode(['price' => round($model->predict([[
+            $_POST['life_sq'],
+            $_POST['full_sq'],
+            $_POST['floor'],
+            $_POST['build_year'],
+        ]])[0])]);
         break;
     default:
         echo 'bad request';
